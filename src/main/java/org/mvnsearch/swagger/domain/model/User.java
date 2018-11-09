3aa91ad28d6fe961dc9f1625ac3aae8fe84bdba1
linux_china <linux_china@hotmail.com>
2018-11-09 09:06:34 -0800

    user class

---
+++ src/main/java/org/mvnsearch/swagger/domain/model/User.java
@@ -0,0 +1,47 @@
+package org.mvnsearch.swagger.domain.model;
+
+import java.util.Date;
+
+/**
+ * user
+ *
+ * @author linux_china
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
