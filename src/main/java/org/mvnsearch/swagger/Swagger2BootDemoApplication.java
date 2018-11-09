598f2f6e19f38fc5acd6bc3cbf0e04db1e200345
linux_china <linux_china@hotmail.com>
2018-11-09 09:03:22 -0800

    boot application

---
+++ src/main/java/org/mvnsearch/swagger/Swagger2BootDemoApplication.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2BootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Swagger2BootDemoApplication.class, args);
+    }
+
+}
